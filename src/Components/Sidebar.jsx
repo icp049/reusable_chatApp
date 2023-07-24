7d216fdd8cb7b8f8b4c3974e0955d00411cfba58
icp049 <ianpepe2000@gmail.com>
2023-07-24 00:28:45 -0600

    prototyping navbar

--- src/Components/Sidebar.jsx
+++ src/Components/Sidebar.jsx
@@ -1,8 +1,13 @@
 import React from "react"
+import Navbar from "./Navbar"
 
 const Sidebar = () => {
     return (
-        <div className = "sidebar">Sidebar</div>
+        <div className = "sidebar">
+          <Navbar/>
+
+
+        </div>
     )
 }
 
