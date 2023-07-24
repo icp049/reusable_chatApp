e40de4824cbaa2881b2acf34ae29d204b9a04db5
icp049 <ianpepe2000@gmail.com>
2023-07-24 00:42:01 -0600

    prototyping search

---
+++ src/Components/Navbar.jsx
@@ -0,0 +1,15 @@
+import React from "react"
+
+
+const Navbar = () => {
+    return(
+        <div className = "navbar">
+            <span className = "logo">Chat</span>
+            <div className= "user">
+
+            </div>
+        </div>
+    )
+}
+
+export default Navbar;
