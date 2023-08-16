5305812b5b63a3e306491c70790ff336dc044ccc
icp049 <ianpepe2000@gmail.com>
2023-08-15 21:42:00 -0600

    fixed login

--- src/Pages/Login.jsx
+++ src/Pages/Login.jsx
@@ -1,28 +1,39 @@
-import React from "react";
-
+import React, { useState } from "react";
+import { useNavigate, Link } from "react-router-dom";
+import { signInWithEmailAndPassword } from "firebase/auth";
+import { auth } from "../firebase";
 
 const Login = () => {
-    return (
+  const [err, setErr] = useState(false);
+  const navigate = useNavigate();
 
-        <div className = "formContainer"> 
-        <div className = "formWrapper"> 
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+    const email = e.target[0].value;
+    const password = e.target[1].value;
 
-        <span className="title" > Login</span>
-        <form>
-            <input type = "text" placeholder = "email" />
-            <input type = "text" placeholder = "password" />
-            
-            <button> Login</button>
+    try {
+      await signInWithEmailAndPassword(auth, email, password);
+      navigate("/")
+    } catch (err) {
+      setErr(true);
+    }
+  };
+  return (
+    <div className="formContainer">
+      <div className="formWrapper">
+        <span className="logo">Lama Chat</span>
+        <span className="title">Login</span>
+        <form onSubmit={handleSubmit}>
+          <input type="email" placeholder="email" />
+          <input type="password" placeholder="password" />
+          <button>Sign in</button>
+          {err && <span>Something went wrong</span>}
         </form>
-
-        <p>Don't have an account? Register</p>
-        
-        
-        
-        </div>
-        </div>
-
-    )
-}
+        <p>You don't have an account? <Link to="/register">Register</Link></p>
+      </div>
+    </div>
+  );
+};
 
 export default Login;
