74d3b5b6fe76614638f75944ca99551e4a84f007
icp049 <ianpepe2000@gmail.com>
2023-08-15 21:35:14 -0600

    edited components

--- src/App.js
+++ src/App.js
@@ -1,11 +1,12 @@
 import Register from "./Pages/Register";
 import Login from "./Pages/Login";
+import Home from "./Pages/Home";
 import "./style.scss"
 
 
 function App() {
   return (
-   <Register />
+   <Home/>
   );
 }
 
--- src/Pages/Home.jsx
+++ src/Pages/Home.jsx
@@ -1,4 +1,5 @@
 import React from "react"
+import Sidebar from "../Components/Sidebar";
 
 const Home = () => {
     return (
@@ -10,3 +11,5 @@ const Home = () => {
             </div>
     )
 }
+
+export default Home;
