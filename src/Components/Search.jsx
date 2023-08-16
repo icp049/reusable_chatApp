b57df29766594d3457bf83bd97169ad116dc4589
icp049 <ianpepe2000@gmail.com>
2023-08-15 21:24:55 -0600

    image

--- src/Components/Search.jsx
+++ src/Components/Search.jsx
@@ -8,9 +8,9 @@ const Search = () => {
 
             </div>
             <div className="userChat">
-                <img src = "" />
+                <img src = "https://img.freepik.com/premium-vector/man-avatar-profile-picture-vector-illustration_268834-538.jpg " />
                 <div className="userChatInfo">
-                    <span> </span>
+                    <span>Jane </span>
                 </div>
             </div>
         </div>
