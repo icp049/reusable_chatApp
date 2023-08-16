678482bf7efb6c3e616ee6d476f50e53fffa30ad
icp049 <ianpepe2000@gmail.com>
2023-08-15 21:28:33 -0600

    edited css

--- src/Components/Chats.jsx
+++ src/Components/Chats.jsx
@@ -2,7 +2,19 @@ import React from "react"
 
 const Chat = () => {
     return (
-        <div className = "chats">Chats</div>
+        <div className = "chats">
+            <div className="userChat">
+                <img src = "https://img.freepik.com/premium-vector/man-avatar-profile-picture-vector-illustration_268834-538.jpg " />
+                
+                
+                <div className="userChatInfo">
+                    <span>Jane </span>
+                    <p> hello</p>
+                </div>
+
+
+            </div>
+            </div>
     )
 }
 
