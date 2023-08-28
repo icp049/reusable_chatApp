5eb5c146c1e5de30d49f7153759e7913984615c0
icp049 <ianpepe2000@gmail.com>
2023-08-28 00:13:19 -0600

    added my profile

--- src/pages/MyProfile.jsx
+++ src/pages/MyProfile.jsx
@@ -1,47 +1,110 @@
-import React, { useEffect, useState } from "react";
-import { useContext } from "react";
+import React, { useContext } from "react";
+import { Link } from "react-router-dom";
 import { AuthContext } from "../context/AuthContext";
-import {Link} from "react-router-dom";
 import Landingpagenavbar from "../navbars/Landingpagenavbar";
+import Box from "@mui/material/Box";
+import Typography from "@mui/material/Typography";
 
 const MyProfile = () => {
-    const { currentUser } = useContext(AuthContext);
+  const { currentUser } = useContext(AuthContext);
+
+  // Add your query to fetch additional user details here
+  // const userDetailsQuery = useUserDetailsQuery(currentUser.id);
 
   const getGreetingMessage = () => {
     if (currentUser) {
-      // If a user is logged in, show the greeting with the username
-      return `@ ${currentUser.displayName || "user"}`;
+      return `Hi, ${currentUser.displayName || "there"}`;
     } else {
-      // If no user is logged in, show a generic message
       return "Welcome!";
     }
   };
 
   return (
-    <div>
+    <Box>
       <Landingpagenavbar />
-      <div style={{ display: "flex", flexDirection: "column", justifyContent: "center", alignItems: "center", height: "100vh" }}>
-        <div style={{ position: "relative", marginBottom: "20px" }}>
+      <Box
+        sx={{
+          display: "flex",
+          flexDirection: "row",
+          alignItems: "flex-start",
+          padding: "20px",
+          gap: "40px", // Add spacing between columns
+        }}
+      >
+        {/* Left Column: Profile Picture */}
+        <Box
+          sx={{
+            flex: "0 0 auto",
+            display: "flex",
+            flexDirection: "column",
+            alignItems: "center",
+            gap: "20px",
+          }}
+        >
           <img
             src={currentUser.photoURL}
-            alt=""
+            alt="Profile"
             style={{
-              width: "150px",
-              height: "150px",
+              width: "400px", // Increase the size of the profile picture
+              height: "400px", // Increase the size of the profile picture
               borderRadius: "50%",
               border: "2px solid #fff",
             }}
           />
-        </div>
-        <h1 style={{ marginBottom: "20px" }}>{getGreetingMessage()}</h1>
-        <div>
-          <Link to="/message">
-            <h1>View Messages</h1>
-          </Link>
-        </div>
-      </div>
-    </div>
+          <Typography variant="h4">{getGreetingMessage()}</Typography>
+          {/* Small Details */}
+          <Typography variant="body1">
+            {currentUser.email || "No email available"}
+          </Typography>
+          {/* Add more small details here */}
+        </Box>
+
+        {/* Right Column: Description, Interests, etc. */}
+        <Box
+          sx={{
+            flex: "1 1 auto",
+            display: "flex",
+            flexDirection: "column",
+            gap: "20px",
+          }}
+        >
+          {/* About Me */}
+          <Box>
+            <Typography variant="h5">About Me</Typography>
+            {/* Add query result here */}
+            {/* <Typography>
+              {userDetailsQuery.data?.aboutMe || ""}
+            </Typography> */}
+          </Box>
+
+          {/* Roommate Preferences */}
+          <Box>
+            <Typography variant="h5">Roommate Preferences</Typography>
+            {/* Add query result here */}
+            {/* <Typography>
+              {userDetailsQuery.data?.roommatePreferences || ""}
+            </Typography> */}
+          </Box>
+
+          {/* Interests */}
+          <Box>
+            <Typography variant="h5">Interests</Typography>
+            {/* Add query result here */}
+            {/* <Typography>
+              {userDetailsQuery.data?.interests || ""}
+            </Typography> */}
+          </Box>
+
+          {/* Edit Profile Link */}
+          <Box sx={{ marginTop: "20px" }}>
+            <Link to="/edit-profile">
+              <button>Edit My Profile</button>
+            </Link>
+          </Box>
+        </Box>
+      </Box>
+    </Box>
   );
-        };  
+};
 
 export default MyProfile;
