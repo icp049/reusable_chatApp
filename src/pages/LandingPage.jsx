c25c2062e833f7a00637610b5bfc61305d4c4273
icp049 <ianpepe2000@gmail.com>
2023-08-28 01:29:34 -0600

    added searchbar

--- src/pages/LandingPage.jsx
+++ src/pages/LandingPage.jsx
@@ -1,9 +1,22 @@
-import react from "react";
+import React from "react";
 import Landingpagenavbar from "../navbars/Landingpagenavbar";
+
 const LandingPage = () => {
-    return(
-        <div style={{ display: "flex", flexDirection: "column", justifyContent: "center", alignItems: "center", height: "100vh" }}>
+    return (
+        <div style={{ display: "flex", flexDirection: "column", justifyContent: "flex-start", alignItems: "center", height: "100vh" }}>
             <Landingpagenavbar/>
+            <div style={{ marginTop: "60px", position: "relative" }}>
+                <input
+                    type="text"
+                    placeholder="Search..."
+                    style={{
+                        padding: "10px",
+                        borderRadius: "5px",
+                        border: "1px solid #ccc",
+                        width: "300px",
+                    }}
+                />
+            </div>
             <h1 style={{ marginBottom: "20px" }}>Place Nest HomePage here</h1>
         </div>
     );
