d5d3c9744e1a26b61c91093423b670e1f46b8811
icp049 <ianpepe2000@gmail.com>
2023-08-28 01:31:57 -0600

    added create listing button

--- src/pages/LandingPage.jsx
+++ src/pages/LandingPage.jsx
@@ -3,9 +3,9 @@ import Landingpagenavbar from "../navbars/Landingpagenavbar";
 
 const LandingPage = () => {
     return (
-        <div style={{ display: "flex", flexDirection: "column", justifyContent: "flex-start", alignItems: "center", height: "100vh" }}>
+        <div style={{ position: "relative", minHeight: "100vh" }}>
             <Landingpagenavbar/>
-            <div style={{ marginTop: "60px", position: "relative" }}>
+            <div style={{ display: "flex", justifyContent: "center", alignItems: "center", marginTop: "60px" }}>
                 <input
                     type="text"
                     placeholder="Search..."
@@ -17,7 +17,23 @@ const LandingPage = () => {
                     }}
                 />
             </div>
-            <h1 style={{ marginBottom: "20px" }}>Place Nest HomePage here</h1>
+            <h1 style={{ marginBottom: "20px", textAlign: "center" }}>Place Nest HomePage here</h1>
+            
+            <div
+                style={{
+                    position: "fixed",
+                    bottom: "20px",
+                    right: "20px",
+                    backgroundColor: "blue",
+                    color: "white",
+                    padding: "10px 20px",
+                    borderRadius: "5px",
+                    cursor: "pointer",
+                    boxShadow: "0 2px 5px rgba(0, 0, 0, 0.1)",
+                }}
+            >
+                Add Listing
+            </div>
         </div>
     );
 };
