55ae77a4481332d1e3aa985acfb2716ffc5c9b55
icp049 <ianpepe2000@gmail.com>
2023-08-31 03:59:46 -0600

    arrows in maincarousel will only show during hover

--- src/pages/MainCarousel.jsx
+++ src/pages/MainCarousel.jsx
@@ -1,75 +1,86 @@
-import React from "react";
+import React, { useState } from "react";
 import { Carousel } from "react-responsive-carousel";
 import "react-responsive-carousel/lib/styles/carousel.min.css";
 import NavigateBeforeIcon from "@mui/icons-material/NavigateBefore";
 import NavigateNextIcon from "@mui/icons-material/NavigateNext";
 
 const MainCarousel = ({ photos }) => {
+    const [isCarouselHovered, setIsCarouselHovered] = useState(false);
+
     const handlePrevArrowClick = (e, onClickHandler) => {
-        e.preventDefault(); // Prevent default link behavior
-        e.stopPropagation(); // Stop event propagation
+        e.preventDefault();
+        e.stopPropagation();
         onClickHandler();
     };
 
     const handleNextArrowClick = (e, onClickHandler) => {
-        e.preventDefault(); // Prevent default link behavior
-        e.stopPropagation(); // Stop event propagation
+        e.preventDefault();
+        e.stopPropagation();
         onClickHandler();
     };
 
     return (
-        <Carousel
-            infiniteLoop={true}
-            showThumbs={false}
-            showIndicators={false}
-            showStatus={false}
-            renderArrowPrev={(onClickHandler) => (
-                <div
-                    onClick={(e) => handlePrevArrowClick(e, onClickHandler)}
-                    style={{
-                        position: "absolute",
-                        top: "50%",
-                        left: "0",
-                        color: "white",
-                        padding: "5px",
-                        zIndex: "2",
-                    }}
-                >
-                    <NavigateBeforeIcon sx={{ fontSize: 20 }} />
-                </div>
-            )}
-            renderArrowNext={(onClickHandler) => (
-                <div
-                    onClick={(e) => handleNextArrowClick(e, onClickHandler)}
-                    style={{
-                        position: "absolute",
-                        top: "50%",
-                        right: "0",
-                        color: "white",
-                        padding: "5px",
-                        zIndex: "2",
-                    }}
-                >
-                    <NavigateNextIcon sx={{ fontSize: 20 }} />
-                </div>
-            )}
+        <div
+            onMouseEnter={() => setIsCarouselHovered(true)}
+            onMouseLeave={() => setIsCarouselHovered(false)}
+            style={{ position: "relative" }}
         >
-            {photos.map((photo, index) => (
-                <div key={`carousel-image-${index}`}>
-                    <img
-                        src={photo}
-                        alt={`carousel-${index}`}
+            <Carousel
+                infiniteLoop={true}
+                showThumbs={false}
+                showIndicators={false}
+                showStatus={false}
+                renderArrowPrev={(onClickHandler) => (
+                    <div
+                        onClick={(e) => handlePrevArrowClick(e, onClickHandler)}
                         style={{
-                            width: "100%",
-                            height: "300px",
-                            objectFit: "cover",
-                            backgroundAttachment: "fixed",
-                           
+                            position: "absolute",
+                            top: "50%",
+                            left: "0",
+                            color: "white",
+                            padding: "5px",
+                            zIndex: "2",
+                            opacity: isCarouselHovered ? 1 : 0,
+                            transition: "opacity 0.3s ease-in-out",
                         }}
-                    />
-                </div>
-            ))}
-        </Carousel>
+                    >
+                        <NavigateBeforeIcon sx={{ fontSize: 20 }} />
+                    </div>
+                )}
+                renderArrowNext={(onClickHandler) => (
+                    <div
+                        onClick={(e) => handleNextArrowClick(e, onClickHandler)}
+                        style={{
+                            position: "absolute",
+                            top: "50%",
+                            right: "0",
+                            color: "white",
+                            padding: "5px",
+                            zIndex: "2",
+                            opacity: isCarouselHovered ? 1 : 0,
+                            transition: "opacity 0.3s ease-in-out",
+                        }}
+                    >
+                        <NavigateNextIcon sx={{ fontSize: 20 }} />
+                    </div>
+                )}
+            >
+                {photos.map((photo, index) => (
+                    <div key={`carousel-image-${index}`}>
+                        <img
+                            src={photo}
+                            alt={`carousel-${index}`}
+                            style={{
+                                width: "100%",
+                                height: "300px",
+                                objectFit: "cover",
+                                backgroundAttachment: "fixed",
+                            }}
+                        />
+                    </div>
+                ))}
+            </Carousel>
+        </div>
     );
 };
 
