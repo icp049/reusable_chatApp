ec76131c91252fa0093115ca1bf813d1a736139c
icp049 <ianpepe2000@gmail.com>
2023-09-02 01:34:07 -0600

    login modified

--- src/pages/Login.jsx
+++ src/pages/Login.jsx
@@ -22,7 +22,7 @@ const Login = () => {
   return (
     <div className="formContainer">
       <div className="formWrapper">
-        <span className="logo">Prototype</span>
+        <span className="logo">NestMates</span>
         <span className="title">Login</span>
         <form onSubmit={handleSubmit}>
           <input type="email" placeholder="email" />
