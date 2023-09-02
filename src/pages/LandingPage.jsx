29b7ccaffb2f870f50dd6edb2f3d41b7f04f6f2c
icp049 <ianpepe2000@gmail.com>
2023-09-01 22:37:45 -0600

    autofill integrated for grid

--- src/pages/LandingPage.jsx
+++ src/pages/LandingPage.jsx
@@ -116,7 +116,7 @@ const LandingPage = () => {
                 <div
                     style={{
                         display: "grid",
-                        gridTemplateColumns: "repeat(auto-fit, minmax(300px, 1fr))",
+                        gridTemplateColumns: "repeat(auto-fill, minmax(300px, 1fr))",
                         gap: "20px",
                         padding: "40px 20px", // Adjust padding
                         justifyContent: "center", // Center the items
