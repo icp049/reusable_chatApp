65bc6e2caafce1853de76914238b1c4480f305f6
IJP <108185265+icp049@users.noreply.github.com>
2023-10-09 23:46:31 -0600

    Update Login.jsx

--- src/pages/Login.jsx
+++ src/pages/Login.jsx
@@ -2,7 +2,7 @@ import React, { useState } from "react";
 import { useNavigate, Link } from "react-router-dom";
 import { signInWithEmailAndPassword } from "firebase/auth";
 import { auth } from "../firebase";
-
+//const
 const Login = () => {
   const [err, setErr] = useState(false);
   const navigate = useNavigate();
